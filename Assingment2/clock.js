09da102db8f09f48824782af2a25540ce9431b62
soonniee <ckstns1231@gmail.com>
2021-04-20 11:13:08 +0900

    step5

--- Assingment2/clock.js
+++ Assingment2/clock.js
@@ -1,6 +1,10 @@
 const header = document.querySelector("header"),
-    digital_time = document.querySelector(".digital__time"),
-    digital_btn = document.querySelector(".digital__btn"); 
+    digitalTime = document.querySelector(".digital__time"),
+    digitalBtn = document.querySelector(".digital__btn"),
+    analogHour = document.querySelector(".analog__hour"),
+    analogMinute = document.querySelector(".analog__minute"),
+    analogSecond = document.querySelector(".analog__second");
+let hour24 = false;
 const getTime = () =>{
     const now = new Date();
     
@@ -24,24 +28,51 @@ const getTime = () =>{
         "November",
         "December",
     ];
+    let time;
     month = monthList[month];
-    if(hour >=0 && hour<=11){
-        if(hour === 0) hour = 12;
-        digital_btn.innerHTML = "AM";
-    }else{
-        if(hour > 12) hour -= 12;
-        digital_btn.innerHTML = "PM";
+    if(!hour24){
+        if(hour >=0 && hour<=11){
+            if(hour === 0) hour = 12;
+            
+            time = "AM";
+        }else{
+            if(hour > 12) hour -= 12;
+            
+            time = "PM";
+        }
+        digitalBtn.innerHTML = "24H";
     }
-    return {year,month,day,hour,minute,second};
+    else time = "";
+    return {year,month,day,hour,minute,second,time};
+};
+const paintClock = (hour,minute,second) =>{
+    const hourDegree = hour * (360/12)+90,
+        minuteDegree = minute * (360/60)+90 ,
+        secondDegreee = second * (360/60) +90;
+    analogHour.style.transform = `rotate(${hourDegree}deg)`;
+    analogMinute.style.transform = `rotate(${minuteDegree}deg)`;
+    analogSecond.style.transform = `rotate(${secondDegreee}deg)`;
 };
 const paintTime = () =>{
-    const {year, month, day, hour, minute, second} = getTime();
+    const {year, month, day, hour, minute, second,time} = getTime();
     
-    header.innerHTML = `Today is <span>${day} ${month}, ${year}</span>`;
-    digital_time.innerHTML = `${hour < 10 ? `0${hour}` : hour} : ${minute < 10 ? `0${minute}` : minute} : ${second < 10 ? `0${second}` : second}`;
+    header.innerHTML = `<span> ${day} ${month}, ${year}</span>`;
+    digitalTime.innerHTML = `${time}   ${hour < 10 ? `0${hour}` : hour} : ${minute < 10 ? `0${minute}` : minute} : ${second < 10 ? `0${second}` : second}`;
+    paintClock(hour,minute,second);
+};
+
+const changeTime = () =>{
+    if(hour24){
+        hour24 = false;
+       
+    }else{
+        hour24 = true;
+         digitalBtn.innerHTML="12H";
+    }
 };
 const init = () =>{
     setInterval(paintTime,1000);
+    digitalBtn.addEventListener("click",changeTime);
 };
 init();
 
--- Assingment2/weather.js
+++ Assingment2/weather.js
@@ -1,14 +1,55 @@
+const weatherMain = document.querySelector(".info__main"),
+    weatherTemp = document.querySelector(".info__temp"),
+    detailTemps = document.querySelector(".detail__temp"),
+    detailWeathers = document.querySelector(".detail__weather"),
+    weatherImg = document.querySelector('.weather__img'),
+    place = document.querySelector(".info__place");
+
 const API_KEY = 'ad6b4834c23119a6e1cc84833e607c15';
 const LOC = "location";
 const saveLoc = (locObj) =>{
     localStorage.setItem(LOC,JSON.stringify(locObj));
+}
+const paintWeather = (weatherObj) =>{
+    weatherImg.innerHTML = `<img src="http://openweathermap.org/img/wn/${weatherObj.img}@2x.png" alt="icon" />`;
+    weatherMain.innerHTML=`${weatherObj.main} `;
+    weatherTemp.innerHTML=`${weatherObj.temp} °C `;
+    detailTemps.innerHTML=`<span>Feels : ${weatherObj.tempFeel} °C </span>
+                            <span>Min : ${weatherObj.tempMin} °C </span>
+                            <span>Max : ${weatherObj.tempMax} °C </span>`;
+    place.innerHTML = `${weatherObj.place}, `;
+    if(weatherObj.rain){
+        detailWeathers.innerHTML=`<span>Humidity : ${weatherObj.humidity} % </span>
+                            <span>Wind : ${weatherObj.wind} m/s </span>
+                            <span>Rain : ${weatherObj.rain} mm/h </span>`;
+                            
+    }
+    else{
+        detailWeathers.innerHTML=`<span>Humidity : ${weatherObj.humidity} % </span>
+                            <span>Wind : ${weatherObj.wind} m/s </span>`;
+                            
+    }
 }
 const getWeather = async(lat,lon) =>{
     const data = await fetch(
         `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
     );
     const weatherData = await data.json();
-    console.log(weatherData);
+    const weatherObj = {
+        main : weatherData.weather[0].main,
+        temp : weatherData.main.temp,
+        tempFeel : weatherData.main.feels_like,
+        tempMin : weatherData.main.temp_min,
+        tempMax : weatherData.main.temp_max,
+        humidity : weatherData.main.humidity,
+        wind: weatherData.wind.speed,
+        id : weatherData.weather[0].main,
+        rain : weatherData.rain ? weatherData.rain["1h"] : null,
+        img: weatherData.weather[0].icon,
+        place : weatherData.name,
+    }
+    
+    paintWeather(weatherObj);
 }
 const handleSuccess = (position) =>{
     const { latitude, longitude } = position.coords;
